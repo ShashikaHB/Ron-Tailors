6c63d6ca52b8d8a76fd49d257bcb22fcbe5152d5
Shashika Harshana <h.shashika@gtngroup.com>
2024-05-18 21:14:10 +0530

    Initial commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+function App() {
+
+
+  return (
+    <>
+     <Button variant="primary">Click Me</Button>
+     <Button variant="success">Click Me</Button>
+    </>
+  )
+}
+
+export default App
