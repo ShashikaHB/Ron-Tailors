285c2b3bb2a9949edf6dcf232ca0ccb8ae30c8c3
Shashika Harshana <s.harshana@gtngroup.com>
2024-10-18 12:50:24 +0530

    cors update

--- Backend/config/cors/allowedOrigins.js
+++ Backend/config/cors/allowedOrigins.js
@@ -3,7 +3,7 @@ export const allowedOrigins = [
   "http://localhost:5174",
   "https://rontailors.netlify.app",
   "https://ron-tailors.netlify.app",
-  "https://ron-tailors-official.netlify.app/login",
+  "https://ron-tailors-official.netlify.app",
   "https://ron-tailors.lk"
 
 ];
